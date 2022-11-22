fd734705eb718a47e9128f8c546190356d3fc1de
tiwarirr <tiwari.radharaman@gmail.com>
2022-11-22 12:46:27 +0530

    First View Added

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hi you are at Mysite's Poll App Landing Page")
 
 # Create your views here.
