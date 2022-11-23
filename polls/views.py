c0ddf997f1524e067b239050c5033906defe6216
tiwarirr <tiwari.radharaman@gmail.com>
2022-11-23 10:16:39 +0530

    added some views

--- polls/views.py
+++ polls/views.py
@@ -5,3 +5,13 @@ def index(request):
     return HttpResponse("Hi you are at Mysite's Poll App Landing Page")
 
 # Create your views here.
+def detail(request,question_id):
+    return HttpResponse("You are looking at question %s. " % question_id)
+
+def results(request,question_id):
+    response = "You are looking at the result of question %s. "
+    return HttpResponse(response % question_id)
+
+def vote(request,question_id):
+    return HttpResponse("You are voting on question %s. " % question_id)
+
