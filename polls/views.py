b213b8f11eea3c9b49b02915ab8cbd58fcfa3617
tiwarirr <tiwari.radharaman@gmail.com>
2022-11-23 13:12:07 +0530

    url added

--- polls/views.py
+++ polls/views.py
@@ -1,20 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from .models import Question
 from django.template import loader
+from django.http import Http404
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list':latest_question_list,
         }
-    return HttpResponse(template.render(context,request))
+    return render(request,'polls/index.html', context)
 
 
 # Create your views here.
 def detail(request,question_id):
-    return HttpResponse("You are looking at question %s. " % question_id)
+    question = get_object_or_404(Question,pk=question_id)
+    return render (request, 'polls/detail.html',{'question':question})
 
 def results(request,question_id):
     response = "You are looking at the result of question %s. "
